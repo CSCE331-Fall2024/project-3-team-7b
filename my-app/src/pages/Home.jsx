460a7556681b6bba2459f1b34a1e0be2c610f7bb
alissapark <alissapark24@gmail.com>
2024-10-23 18:07:45 -0500

    basic customer home page

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -1,10 +1,25 @@
 import banner from "../images/banner.PNG"
 import Button from '@mui/material/Button';
 import { ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from "react-router-dom";
 import "./Home.css"
 import theme from "../createTheme"
 
 function Home(){
+    const navigate = useNavigate();
+    
+    const handleCustomerView = () => {
+        navigate("/customer");
+    }
+    const handleCashierView = () => {
+        navigate("/cashier");
+    }
+    const handleManagerView = () => {
+        navigate("/manager");
+    }
+    const handleMenuView = () => {
+        navigate("/menu");
+    }
     return (
         <ThemeProvider theme={theme}>
             <div>
@@ -13,10 +28,18 @@ function Home(){
                 </div>
 
                 <div className="button-div">
-                    <Button variant="contained">Customer View</Button>
-                    <Button variant="contained">Cashier View</Button>
-                    <Button variant="contained">Manager View</Button>
-                    <Button variant="contained">Menu View</Button>
+                    <Button className="home-buttons" variant="contained" onClick={handleCustomerView}>
+                        Customer View
+                    </Button>
+                    <Button className="home-buttons" variant="contained" onClick={handleCashierView}>
+                        Cashier View
+                        </Button>
+                    <Button className="home-buttons" variant="contained" onClick={handleManagerView}>
+                        Manager View
+                        </Button>
+                    <Button className="home-buttons" variant="contained" onClick={handleMenuView}>
+                        Menu View
+                        </Button>
                 </div>
             </div>
        </ThemeProvider>
---
+++ my-app/src/pages/customer/CustomerHome.jsx
@@ -0,0 +1,44 @@
+import logo from "../../images/logo.png"
+import Button from '@mui/material/Button';
+import { ThemeProvider } from '@mui/material/styles';
+import { useNavigate } from "react-router-dom";
+import "./customer.css"
+import theme from "../../createTheme"
+
+function CustomerHome(){
+    const navigate = useNavigate();
+    
+    const startOrder = () => {
+        navigate("/customer/order");
+    }
+
+    return (
+        <ThemeProvider theme={theme}>
+            <div>
+                <div>
+                    <Button className="translate-button" variant="contained">Select A Language</Button>
+                </div>
+
+                <div className="content-div">
+                    <div className="logo-div">
+                        <img className="logo" src={logo} alt="Panda Express Banner"></img>
+                    </div>
+                    <div>
+                    <Button className="order-button" variant="contained" onClick={startOrder}>Start Order</Button>
+                    </div>
+                    <div className="weather-div">
+                        <h1>Today's Weather:</h1>
+                        <div>
+                            <p>Something that shows the current weather</p>
+                        </div>
+                        <div>
+                            <h3>Item of the Day:</h3>
+                        </div>
+                    </div>
+                </div>
+            </div>
+       </ThemeProvider>
+    );
+}
+
+export default CustomerHome;
