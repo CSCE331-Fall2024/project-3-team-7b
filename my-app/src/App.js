10d3b847a609371d9d1f2286ba478bd6ecd309e4
alissapark <alissapark24@gmail.com>
2024-10-23 14:31:06 -0500

    added organization

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import './App.css';
-import Login from './Login';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<Login />}/>
+          <Route path="" element={<Login />}/>
         </Routes>
       </BrowserRouter>
     </div>
