b806b306448017776090a3223acf6860c70d1b9a
Andy Sun <andy.sun1104@gmail.com>
2024-11-05 17:40:53 -0600

    render debugging

--- backend/index.js
+++ backend/index.js
@@ -54,6 +54,6 @@ app.get('*', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
