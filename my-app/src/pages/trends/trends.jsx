6c9e386c549becfbc7e63266a66d05bb32ed3db1
alissapark <alissapark24@gmail.com>
2024-11-11 14:40:01 -0600

    added dropdown to manager trends view

--- my-app/src/pages/trends/trends.jsx
+++ my-app/src/pages/trends/trends.jsx
@@ -2,6 +2,7 @@ import ManagerBanner from '../manager/managerBanner';
 import { ThemeProvider } from '@mui/material/styles';
 // import styles from "./trends.module.css";
 import theme from "../../createTheme"
+import { Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 
 function Trends(props){
     const view = props.view;
@@ -11,6 +12,18 @@ function Trends(props){
         <ThemeProvider theme={theme}>
             <div>
                 <ManagerBanner view={view} setAuthentication={setAuthentication}/>
+                <FormControl fullWidth>
+                    <InputLabel>Select A Trend</InputLabel>
+                    <Select
+                        label="Select A Trend"
+                    >
+                        <MenuItem>Item Performance</MenuItem>
+                        <MenuItem>Product Usage Chart</MenuItem>
+                        <MenuItem>X Report</MenuItem>
+                        <MenuItem>Y Report</MenuItem>
+                        <MenuItem>Sales Report</MenuItem>
+                    </Select>
+                </FormControl>
             </div>
        </ThemeProvider>
     );
