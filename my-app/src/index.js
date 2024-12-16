b506f4f2023c9d573b3fdd87e3fa348fb36799f3
alissapark <alissapark24@gmail.com>
2024-12-16 13:37:11 -0600

    hid the google auth api key

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,11 +8,20 @@ import { EnlargeProvider } from './EnlargeContext';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Access the environment variable
+const googleClientId = process.env.REACT_APP_GOOGLE_AUTH_KEY;
+
+if (!googleClientId) {
+  console.error('Google Auth Client ID is not defined. Please check your .env file.');
+}
+
+
 root.render(
   <Provider store={store}>
     <React.StrictMode>
       <EnlargeProvider>
-        <GoogleOAuthProvider clientId="870342903927-3lqp8kfhv0labcj2a8fg56epsdc2dpmr.apps.googleusercontent.com">
+        <GoogleOAuthProvider clientId={googleClientId}>
           <App />
         </GoogleOAuthProvider>
       </EnlargeProvider>
